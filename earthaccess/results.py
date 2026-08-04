39e9e9d218b5fef7ea6e403293818e70ad616156
Andy Barrett <apbarret@nsidc.org>
2026-08-04 11:46:52 -0600

    change internal calls to size() to private _size

--- earthaccess/results.py
+++ earthaccess/results.py
@@ -345,7 +345,7 @@ class DataGranule(CustomDict):
         super().__init__(collection)
         self.cloud_hosted = cloud_hosted
         # TODO: maybe add area, start date and all that as an instance value
-        self["size"] = self.size()
+        self["size"] = self._size
         self.uuid = str(uuid.uuid4())
         self.render_dict: Any
         if fields is None:
@@ -372,7 +372,7 @@ class DataGranule(CustomDict):
         Collection: {self["umm"]["CollectionReference"]}
         Spatial coverage: {self["umm"]["SpatialExtent"]}
         Temporal coverage: {self["umm"]["TemporalExtent"]}
-        Size(MB): {self.size()}
+        Size(MB): {self._size}
         Data: {data_links}\n\n
         """.strip().replace("  ", "")
 
